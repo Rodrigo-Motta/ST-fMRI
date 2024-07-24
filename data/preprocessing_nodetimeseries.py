304584a0de480d5c2f523d758dd56e2d65ca1cc1
rodrigodamotta <rodrigodamottacc@gmail.com>
2024-07-24 16:24:11 +0100

    updated whole project (working)

---
+++ data/preprocessing_nodetimeseries.py
@@ -0,0 +1,247 @@
+# -*- coding: utf-8 -*-
+# @Author: Simon Dahan @SD3004
+# @Date:   2021-08-26 15:02:02
+# @Last Modified by:   Simon Dahan @SD3004
+# @Last Modified time: 2021-08-31 16:11:26
+
+import os
+import argparse
+
+import numpy as np
+import pandas as pd
+
+from scipy import stats
+from sklearn.model_selection import StratifiedKFold
+
+
+def main(args):
+    """
+    Main function to preprocess timeseries data and compute adjacency matrices.
+
+    Args:
+        args: Parsed command-line arguments containing:
+            - filename: Path to the file with the list of subject IDs (.txt or .csv).
+            - ICA: Number of Independent Component Analysis (ICA) nodes.
+            - data_path: Root directory path where node timeseries data files are stored.
+            - output_folder: Path to the directory where results will be saved.
+
+    Variables:
+        ids: List of subject IDs loaded from the specified file.
+        nb_subject: Total number of subjects derived from the shape of `ids`.
+        ICA_nodes: Number of ICA nodes.
+        data_path: Root directory path where node timeseries data files are stored.
+        T: Temporal length sequence, set to 1200.
+        data: Numpy array initialized to zeros with shape (nb_subject, 1, T, ICA_nodes, 1) to store preprocessed timeseries data for each subject.
+        label: Numpy array initialized to zeros with shape (nb_subject,) to store labels corresponding to each subject.
+        non_used: List to keep track of subject IDs not used due to data issues.
+        subject_string: Formatted string representing the subject ID for constructing file paths.
+        filename: Specific filename for each subject's node timeseries data.
+        filepath: Full path to the node timeseries data file for each subject.
+        full_sequence: Timeseries data for a subject loaded from the file.
+        z_sequence: Z-score normalized timeseries data for a subject.
+        data_all: Concatenated array of z-score normalized timeseries data for all subjects.
+        A: Adjacency matrix computed based on the correlation of node timeseries data.
+        train_data: Subset of `data` used for training obtained from the StratifiedKFold split.
+        train_label: Subset of `label` corresponding to `train_data`.
+        test_data: Subset of `data` used for testing obtained from the StratifiedKFold split.
+        test_label: Subset of `label` corresponding to `test_data`.
+        idx: Index counter used to track the number of successfully processed subjects.
+        df: Pandas DataFrame containing the list of subject IDs not used.
+        skf: Instance of `StratifiedKFold` from `scikit-learn` used to split the data into training and testing sets.
+        fold: Counter to track the fold number in the StratifiedKFold split.
+    """
+
+    ### loading subject list
+    print('')
+    print('#'*30)
+    print('Starting: preprocessing script')
+    print('#'*30)
+    print('')
+
+    if args.filename.find('txt') != -1:
+        ids = np.loadtxt(args.filename)
+        nb_subject = ids.shape[0]
+    elif args.filename.find('csv') != -1: 
+        ids = pd.read_csv(args.filename)
+        nb_subject = ids.shape[0]
+    else:
+        raise TypeError('filetype not implemented')
+
+
+    ICA_nodes = int(args.ICA)
+    data_path = str(args.data_path)
+    
+    T = 110
+    data = np.zeros((nb_subject,1,T,ICA_nodes,1))
+    label = np.zeros((nb_subject,))
+
+    print('')
+    print('Number of subjects: {}'.format(nb_subject))
+    print('ICA nodes: {}'.format(ICA_nodes))
+    print('Temporal length sequence: {}'.format(T))
+    print('')
+
+    ### loading node timeseries
+
+    print('')
+    print('#'*30)
+    print('Loading: node timeseries')
+    print('#'*30)
+    print('')
+
+    idx = 0
+    non_used = []
+    for i in range(nb_subject):
+
+        ## -----------
+        ids.Gender.replace('M', 0, inplace=True)
+        ids.Gender.replace('F', 1, inplace=True)
+        ## --------
+
+        # Original subjects files
+        #subject_string = format(int(ids[i,0]),'06d')
+
+        ## ---------
+        subject_string = format(int(ids.loc[i,'Subject']), '06d')
+        ## --------
+        ## -----------
+        ids.Gender.replace('M', 0, inplace=True)
+        ids.Gender.replace('F', 1, inplace=True)
+        ## --------
+
+        # Original subjects files
+        #subject_string = format(int(ids[i,0]),'06d')
+
+        ## ---------
+        subject_string = format(int(ids.loc[i,'Subject']), '06d')
+        ## --------
+        filename = 'HCP_PTN1200/node_timeseries_{}/3T_HCP1200_MSMAll_d{}_ts2/'.format(ICA_nodes, ICA_nodes)+subject_string+'.txt'
+        filepath = os.path.join(str(data_path),filename)
+        print(filepath)
+        if os.path.exists(filepath):
+            full_sequence = pd.read_csv(filepath, delimiter = " ").to_numpy()[:110,:]
+            full_sequence = pd.read_csv(filepath, delimiter = " ").to_numpy()[:110,:]
+            if full_sequence.shape[0] < T:
+                print('sequence too short :{}'.format(filepath))
+                continue
+
+            full_sequence = np.transpose(full_sequence[:T,:])
+
+            z_sequence = stats.zscore(full_sequence, axis=1)
+            
+            if idx ==0:
+                data_all = z_sequence
+            else:
+                data_all = np.concatenate((data_all, z_sequence), axis=1)
+            
+            data[idx,0,:,:,0] = np.transpose(z_sequence)
+            # Original subjects file
+            #label[idx] = ids[i,1]
+
+            ## --------
+            label[idx] = ids.loc[i, args.label]
+            ## --------
+
+            # Original subjects file
+            #label[idx] = ids[i,1]
+
+            ## --------
+            label[idx] = ids.loc[i, args.label]
+            ## --------
+
+            idx = idx + 1
+            if idx%100==0:
+                print('subject preprocessed: {}'.format(idx))
+            
+        else:
+            non_used.append(subject_string)
+    
+    ### keep only subjects
+    data = data[:idx,0,:,:,0]
+    label = label[:idx]
+
+    print('')
+    print('Number of subjects loaded: {}'.format(idx))
+    print('Data shape: {}'.format(data.shape))
+    print('')
+
+    df = pd.DataFrame(data=np.array(non_used))
+
+    try:
+        os.mkdir(args.output_folder)
+        print('Creating folder: {}'.format(args.output_folder))
+    except OSError:
+        print('folder already exist: {}'.format(args.output_folder))
+
+    df.to_csv(os.path.join(args.output_folder,'ids_not_used.csv'))
+
+    print('')
+    print('#'*30)
+    print('Computing: Adjacency Matrix')
+    print('#'*30)
+    print('')
+           
+    
+    # compute adj matrix
+    n_regions = ICA_nodes
+    A = np.zeros((n_regions, n_regions))
+    for i in range(n_regions):
+        for j in range(i, n_regions):
+            if i==j:
+                A[i][j] = 1
+            else:
+                A[i][j] = abs(np.corrcoef(data_all[i,:], data_all[j,:])[0][1]) # get value from corrcoef matrix
+                A[j][i] = A[i][j]
+    
+
+    print('')
+    print('saving adjacency matrix')
+    print('')
+
+    try:        
+        os.mkdir(os.path.join(args.output_folder,'node_timeseries'))  
+        print('Creating folder: {}'.format(os.path.join(args.output_folder,'node_timeseries')))
+    except OSError:
+        print('folder already exist: {}'.format(os.path.join(args.output_folder,'node_timeseries')))
+
+
+    try:        
+        os.mkdir(os.path.join(args.output_folder,'node_timeseries/node_timeseries_{}'.format(ICA_nodes)))  
+        print('Creating folder: {}'.format(os.path.join(args.output_folder,'node_timeseries/node_timeseries_{}/'.format(ICA_nodes))))
+    except OSError:
+        print('folder already exist: {}'.format(os.path.join(args.output_folder,'node_timeseries/node_timeseries_{}/'.format(ICA_nodes))))
+
+    np.save(os.path.join(args.output_folder,'node_timeseries/node_timeseries_{}/adj_matrix.npy'.format(ICA_nodes)), A)
+
+    # split train/test and save data
+    
+    skf = StratifiedKFold(n_splits=5,shuffle=True)
+    fold = 1
+    for train_idx, test_idx in skf.split(data, label):
+        train_data = data[train_idx]
+        train_label = label[train_idx]
+        test_data = data[test_idx]
+        test_label = label[test_idx] 
+
+        filename = os.path.join(args.output_folder,'node_timeseries/node_timeseries_{}'.format(ICA_nodes), 'train_data_1200_'+str(fold)+'.npy')
+        np.save(filename,train_data)
+        filename = os.path.join(args.output_folder,'node_timeseries/node_timeseries_{}'.format(ICA_nodes),'train_label_1200_'+str(fold)+'.npy')
+        np.save(filename,train_label)
+        filename = os.path.join(args.output_folder,'node_timeseries/node_timeseries_{}'.format(ICA_nodes),'test_data_1200_'+str(fold)+'.npy')
+        np.save(filename,test_data)
+        filename = os.path.join(args.output_folder,'node_timeseries/node_timeseries_{}'.format(ICA_nodes),'test_label_1200_'+str(fold)+'.npy')
+        np.save(filename,test_label)
+
+        fold = fold + 1
+  
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Preprocessing script.")
+    parser.add_argument('--filename', required=True, help='Path to the subject ID file (txt or csv).')
+    parser.add_argument('--ICA', required=True, type=int, help='Number of ICA nodes.')
+    parser.add_argument('--label', required=True, type=str, help='Label name.')
+    parser.add_argument('--data_path', required=True, help='Base directory of node timeseries data.')
+    parser.add_argument('--output_folder', required=True, help='Directory to save the output data.')
+
+    args = parser.parse_args()
+    main(args)
